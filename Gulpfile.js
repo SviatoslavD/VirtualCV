d13e39b4bde4c109fd17d72531100131733d0e0c
SviatoslavD <sviatoslav.denykevych@gmail.com>
2015-11-19 22:34:17 +0200

    create structure

--- Gulpfile.js
+++ Gulpfile.js
@@ -5,7 +5,7 @@ var gulp = require('gulp'),
     fs = require('fs'),
     uglify = require('gulp-uglify'),
     bowerFiles = require('main-bower-files'),
-    extend = require('node.extend'),
+    extend = require('node.extend'),         // A port of jQuery.extend that actually works on node.js
     rename = require("gulp-rename"),
     argv = require("yargs").argv,
     gulpFilter = require('gulp-filter'),
@@ -20,8 +20,7 @@ var gulp = require('gulp'),
     gutil = require('gulp-util'),
     minifyCss = require('gulp-minify-css'),
     nop = require('gulp-nop'),
-    ngHtml2Js = require("gulp-ng-html2js"),
-    angularProtractor = require('gulp-angular-protractor');
+    ngHtml2Js = require("gulp-ng-html2js");
 
 // app main config file
 var config = require('./config.json'),
@@ -49,3 +48,33 @@ if(isBuild) {
 var pathToVendors = path.app + dirVendors;
 
 // update config (combine common with local and env configs)
+config[env].VERSION = buildVersion;
+config[env].DEBUG = !isBuild;
+config[env].ENV = env;
+config[env].STATICDIR = path.urlStatic;
+var appConfig = extend(true, config.common, config[env]);
+var appConfigStr = JSON.stringify(appConfig);
+
+// application
+var app = 'index';
+
+gutil.log('Running env = ' + env);
+gutil.log('[path] src = ' + path.src);
+gutil.log('[path] dist = ' + path.dist);
+gutil.log('[path] app = ' + path.app);
+gutil.log('[path] urlStatic = ' + path.urlStatic);
+gutil.log('[path] distStatic = ' + path.distStatic);
+gutil.log('[path] pathToVendors = ' + pathToVendors);
+gutil.log('[path] pathToBowerJson = ' + pathToBowerJson);
+
+function watchLocalChanges () {
+    
+}
+
+/******* startup taks *******/
+
+// default task
+/*if (isBuild) {
+    gulp.task('default', ['package']);
+} else {
+    gulp.task('default', ['app:html'], watchLocalChanges);*/
